ed60ab1568a5c017aa2f915863d3440cfa1cb6c1
Aiman <ahardao1001@gmail.com>
2025-02-10 12:19:51 +0100

    primer commit proyecto react aiman

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ function App() {
           className='freecodecamp-logo' />
       </div>
       <div className='tareas-lista-principal'>
+	<h1>Aiman</h1>
         <h1>Mis Tareas</h1>
         <ListaDeTareas />
       </div>
